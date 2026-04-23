b6342fe52da59f52d144cdcdc6237b5af66cf12e
DevilsAprentice <nooblucky13@gmail.com>
2026-04-23 21:26:01 +0700

    fix: complete game flow and UI

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;  
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public GameState currentState;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        currentState = GameState.Playing;
+    }
+
+    void Update()
+    {
+       
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        currentState = GameState.Paused;
+        Debug.Log("Game Paused");
+         
+    } 
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        currentState = GameState.Playing;
+        Debug.Log("Resumed");
+    }
+
+public void GameOver()
+    {
+        if (currentState == GameState.GameOver) return;
+
+        currentState = GameState.GameOver;
+        Time.timeScale = 0f; 
+        Debug.Log("GAME OVER");
+    }
+
+}
